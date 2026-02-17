8638afd3fcac46a228137e27629c474ef1b6296e
Mosufettable <mosupet128@gmail.com>
2026-02-17 11:42:38 +0900

    뭐시기뭐시기

--- InterFace.cs
+++ InterFace.cs
@@ -10,17 +10,16 @@ namespace Inte
         public static void Processing()
         {
             Tions.KeyGen();
-
+            Console.WriteLine("=========================================");
             Console.WriteLine($"공개키 (N): {Cryptor.Program.N}");
             Console.WriteLine($"공개키 (e): {Cryptor.Program.e}");
             Console.WriteLine($"개인키 (d): {Cryptor.Program.d}");
-
-            Cryptor.Program.a = 7; // 테스트용 원본 메시지
-            Console.WriteLine($"원본 메시지(테스트로 설정해둔 것): {Cryptor.Program.a}");
-
+            Console.WriteLine("=========================================");
+            Console.WriteLine("\n암호화할 숫자를 입력하시라요...");
+            Cryptor.Program.a = BigInteger.Parse(Console.ReadLine());
             Tions.Encrypt();
             Console.WriteLine($"암호화 결과: {Cryptor.Program.b}");
-
+            Console.WriteLine("\n복호화하시겠습네까...(Y/N)");
             Tions.Decrypt();
             Console.WriteLine($"복호화 결과: {Cryptor.Program.a}");
         }
